79f224c46d1f4c706ff094fdc93d4218543235fb
premvarma2002 <vakilverma1982@gmail.com>
2023-11-23 23:18:42 +0530

    footerlinks append

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
-
+import { footerLinks } from "@/constants"
 
 const Footer = () => {
   return (
@@ -8,7 +8,17 @@ const Footer = () => {
       <div className=" flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
         <div className="flex flex-col justify-start items-start gap-6 ">
          <Image src="/logo.svg" alt="logo" width={118} height={18} className="object-contain"/>
+         <p className="text-base text-gray-700">CarHub 2023 <br/> All Rights Reserved &copy;</p>
         </div>
+       <div className="footer__links">
+        {footerLinks.map((link) => (
+          <div key={link.title}
+          className="footer__link">
+            <h3>{link.title}</h3>
+            </div>
+        ))
+        }
+       </div>
       </div>
 
     </footer>
