777d6a8e1dcafa5ff819d9eb9803b0aec306fec7
premvarma2002 <vakilverma1982@gmail.com>
2023-11-24 23:02:12 +0530

    searchbox functionality

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const CustomFilter = () => {
-  return (
-    <div>CustomFilter</div>
-  )
-}
+  return <div>CustomFilter</div>;
+};
 
-export default CustomFilter
+export default CustomFilter;
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,19 +1,21 @@
-'use client'
+"use client";
 
-import { useState } from "react"
-import SearchManufacturer from "./SearchManufacturer"
+import { useState } from "react";
+import SearchManufacturer from "./SearchManufacturer";
 
 const SearchBar = () => {
-    const [manufacturer , setManufacturer] = useState('')
-    const handleSearch = () => {}
+  const [manufacturer, setManufacturer] = useState("");
+  const handleSearch = () => {};
   return (
-   <form className='searchbar' onSubmit={handleSearch}>
-<div className="searchbar__item">
-    <SearchManufacturer manufacturer={manufacturer}
-    setManufacturer={setManufacturer}/>
-</div>
-   </form>
-  )
-}
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
+    </form>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
