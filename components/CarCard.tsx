21dfe5db783356672617fa59b9a65e2679683de3
premvarma2002 <vakilverma1982@gmail.com>
2023-11-25 21:01:42 +0530

    CarCard append1

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+'use client'
+import Image from "next/image"
+import { useState } from "react"
+import { CarProps } from "@/types"
+import { CustomButton } from "."
+interface CarCardProps {
+  car:CarProps;
+}
 
-const CarCard = () => {
+const CarCard = ({car}: CarCardProps) => {
+  const { city_mpg, year , make , model , transmission , drive} = car ;
   return (
-    <div>CarCard</div>
+    <div className="car-card group">
+      <div className="car-card__content">
+         <h2 className="car-card__content-title">{make} {model}</h2>
+      </div>
+    </div>
   )
 }
 
