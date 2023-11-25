2319a6c78d77d2cd61b98af6f287093906e4501d
premvarma2002 <vakilverma1982@gmail.com>
2023-11-25 23:42:48 +0530

    car details

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,12 +3,16 @@ import Image from "next/image";
 import { useState } from "react";
 import { CarProps } from "@/types";
 import { CustomButton } from ".";
+import { calculateCarRent } from "@/utils";
 interface CarCardProps {
   car: CarProps;
 }
 
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
+
+const carRent = calculateCarRent(city_mpg,year);
+
   return (
     <div className="car-card group">
       <div className="car-card__content">
@@ -16,6 +20,12 @@ const CarCard = ({ car }: CarCardProps) => {
           {make} {model}
         </h2>
       </div>
+
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold">$</span>
+        <span className="self-start text-[14px] font-semibold">{carRent}</span>
+        <span className="self-start text-[14px] font-semibold">/day</span>
+      </p>
     </div>
   );
 };
