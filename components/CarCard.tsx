400df6890efadb2aaf9d16c473ad3d147cba9430
premvarma2002 <vakilverma1982@gmail.com>
2023-12-20 19:56:46 +0530

    with local images

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -30,7 +30,7 @@ const carRent = calculateCarRent(city_mpg,year);
         <span className="self-end text-[14px] font-medium">/day</span>
       </p>
       <div className="relative w-full h-40 my-3 object-contain">
-        <Image src={generateCarImageUrl(car)} alt="car model" fill priority className="object-contain" /> 
+        <Image src="/hero.png" alt="car model" fill priority className="object-contain" /> 
 
       </div>
       <div className="relative flex w-full mt-2">
